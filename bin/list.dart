1d54c5e500962a868f114154756624415d4376e9
BLACK PANDA <blackooioopanda@gmail.com>
2023-10-31 10:07:57 +0600

    1st day commit

---
+++ bin/list.dart
@@ -0,0 +1,7 @@
+void main(){
+    var city=['dhaka','khulna'];
+    print(city);
+    print(city[1]);
+
+
+}
---
+++ bin/map.dart
@@ -0,0 +1,15 @@
+void main(){
+
+    var person={
+        'name':"anupam",
+        'age':23,
+        'city':'dhaka',
+        'height':'5 feet 7 inch'
+    };
+
+    print(person);
+    print(person['name']);
+    print(person['age']);
+    print(person['city']);
+    print(person['height']);
+}
